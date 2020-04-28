a7b61d52e12265962a4dd36a8f1933779ccaf5ed
Mark Cruz <mark@Marks-MBP.home>
2020-04-28 08:42:16 -0400

    debugging register

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,7 @@ const handleRegister = (req, res, knex, bcrypt) => {
     .then(trx.commit)
     .catch(trx.rollback)
   })
-  .catch(err => res.status(400).json('unable to register'))
+  .catch(err => res.status(400).json('something went wrong'))
 }
 
 module.exports= {
